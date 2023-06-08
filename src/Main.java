aab62b09df6d9302903e32fd88d4aa8c34e646be
Mr Hee <matteo.hee@spschools.org>
2023-06-08 09:06:56 -0600

    Moving all save/load functions into an OBJECTIO class.

--- src/Main.java
+++ src/Main.java
@@ -5,70 +5,20 @@ public class Main {
     public static void main(String[] args) {
 
         final String filePath = "H:\\~CS20\\cs20-file-example\\files-using-serial\\src\\savedObjects.txt";
+
         ArrayList<modelKit> collection = new ArrayList<>();
-        //modelKit clownySwordPeople = new modelKit("Age of Sigmar", "Slaanesh Mortals", 5,  40);
-      //  modelKit widowmakers = new modelKit("Warmachine", "Khador", 4, 99);
 
+        //Loads collection with n objects from file
+        collection = objectIO.load(filePath,2);
 
-        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
-            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-
-
-            modelKit temp = (modelKit) objectInputStream.readObject();
-            collection.add(temp);
-             temp = (modelKit) objectInputStream.readObject();
-            collection.add(temp);
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
 
         for (modelKit t: collection
              ) {
             System.out.println(t.toString());
         }
 
-
-
-        //Test input.
-
-
-        //collection.add(clownySwordPeople);
-        //collection.add(widowmakers);
-
-        //System.out.println(clownySwordPeople.toString());
-
-//As an example. Save ClownySwordPeople to a file.
-/*
-        try {
-            FileOutputStream fos = new FileOutputStream(filePath);
-            ObjectOutputStream objectOut = new ObjectOutputStream(fos);
-            objectOut.writeObject(clownySwordPeople);
-            objectOut.close();
-            System.out.println("Successfully Saved");
-
-       } catch (Exception e) {
-            e.printStackTrace();
-        }
-*/
-
-        //Now we should write the whole collection to a file.
-//For each will loop through collection
-            try {
-                FileOutputStream fos = new FileOutputStream(filePath);
-                ObjectOutputStream objectOut = new ObjectOutputStream(fos);
-                for (modelKit thisKit : collection
-                ) {
-
-                    objectOut.writeObject(thisKit);
-
-
-        }
-        objectOut.close();
-        System.out.println("Successfully Saved");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        //Saves collection to file.
+        objectIO.save(filePath, collection);
 
     }
 
---
+++ src/objectIO.java
@@ -0,0 +1,54 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class objectIO {
+
+
+    public static ArrayList<modelKit> load(String filePath, int numberOfObjects){
+
+        ArrayList<modelKit> collection = new ArrayList<>();
+
+        try  {
+            FileInputStream fileInputStream = new FileInputStream(filePath);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+            for (int i = 0; i < numberOfObjects; i++) {
+                modelKit temp = (modelKit) objectInputStream.readObject();
+                collection.add(temp);
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return collection;
+
+    }
+
+    public static void save(String filePath, ArrayList<modelKit> collection){
+        //Saving Objects to file
+        try {
+            FileOutputStream fos = new FileOutputStream(filePath);
+            ObjectOutputStream objectOut = new ObjectOutputStream(fos);
+            for (modelKit thisKit : collection
+            ) {
+
+                objectOut.writeObject(thisKit);
+
+
+            }
+            objectOut.close();
+            System.out.println("Successfully Saved");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
